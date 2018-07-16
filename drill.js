ea3378cc50c9a16813985cb81dd573f9df9dc23b
Shelly Leung <pontificable@yahoo.com>
2018-07-16 11:56:27 -0700

    yearOfBirth

--- drill.js
+++ drill.js
@@ -1,5 +1,7 @@
 
-function whoAmI() {
+function whoAmI(name, age) {
+  let yearOfBirth = 2018 - age;
   console.log('Hi, my name is Chris and I\'m 29 years old');
+  console.log(`I was born in ${yearOfBirth}`);
 }
-whoAmI();
+whoAmI(Chris, 29);
