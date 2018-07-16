ab573a27a00ac10c02ffa7ef3b26bea0850cf7b7
Shelly Leung <pontificable@yahoo.com>
2018-07-16 12:11:57 -0700

    age cannot be negative

--- drill.js
+++ drill.js
@@ -1,12 +1,18 @@
 
+function yearOfBirth(age) {
+  if (age < 0) {
+    throw new Error("Age can not be negative");
+  }
+  else {
+    let year = 2018 - age;
+  }
+}
+
 function whoAmI(name, age) {
    let yob = yearOfBirth(age);
    console.log('Hi, my name is Chris and I\'m 29 years old');
-   console.log(`I was born in ${yearOfBirth}`);
+   console.log(`I was born in ${yob}`);
 }
 
-function yearOfBirth(age) {
-  let year = 2018 - age;
 
-}
-whoAmI("Chris", 29);
+whoAmI("Chris", -29);
