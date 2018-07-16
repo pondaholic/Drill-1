911a0b441a156166f81502aa28cac94a75a2db64
kathyleboeuf <kleboeuf@townisp.com>
2018-07-16 15:03:17 -0400

    func added yob

--- drill.js
+++ drill.js
@@ -1,7 +1,12 @@
 
 function whoAmI(name, age) {
-  let yearOfBirth = 2018 - age;
-  console.log('Hi, my name is Chris and I\'m 29 years old');
-  console.log(`I was born in ${yearOfBirth}`);
+   let yob = yearOfBirth(age);
+   console.log('Hi, my name is Chris and I\'m 29 years old');
+   console.log(`I was born in ${yearOfBirth}`);
 }
-whoAmI(Chris, 29);
+
+function yearOfBirth(age) {
+  let year = 2018 - age;
+
+}
+whoAmI("Chris", 29);
